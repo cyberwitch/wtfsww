4f84028c31d422e34dcf659aef71d69ab3f15687
Danielle Weisz <dani@cyberwit.ch>
2015-07-09 21:15:37 -0700

    Move static files out of the python repo

--- wtfsww.py
+++ wtfsww.py
@@ -2,17 +2,14 @@ from json import dumps
 
 from flask import Flask, render_template, request, send_from_directory
 
+from flask_cors import CORS
+
 from tmdbutils import TMDBUtils
 
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
+cors = CORS(app)
 tmdb = TMDBUtils('1c9fdf67f8e8f9df79a09809f463bc25')
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def index(path):
-    return send_from_directory(app.static_folder, 'index.html')
 
 
 @app.route('/api/v1.0/movies', methods=['GET'])
